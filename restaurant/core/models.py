12e4bab08b8138e71fad9de653bdfc46acbc6896
mattjneuber <mattjn@gmail.com>
2015-03-29 19:31:02 -0500

    Mostly done
    I think this takes care of most of the models.

--- restaurant/core/models.py
+++ restaurant/core/models.py
@@ -27,7 +27,7 @@ class MenuItem(models.Model):
 	ingredients = models.ManyToManyField(Ingredient) # this will be used to remove items with ingredients that are out of stock from listing
 	category = models.ForeignKey(Category)
 	nutritional_info = models.URLField()
-	is_vegetarian = models.BooleanField()
+	is_vegetarian = models.BooleanField(default=False)
 
 	ALLERGY_TYPES = (
 		("PEANUT", 'Peanuts'),
@@ -44,6 +44,7 @@ class Table(models.Model):
 		("OP", "Open"),
 		("OC", "Occupied"),
 		("NR", "Need refill"),
+		("NA", "Need assistance"),
 		("PL", "Order placed"),
 	)
 	status = models.CharField(max_length=2, choices=STATUS_TYPES)
@@ -60,6 +61,51 @@ class Order(models.Model):
 	#order_number = models.AutoField() # auto increments 
 	table = models.ForeignKey(Table)
 	menu_items = models.ManyToManyField(MenuItem)
+	date = models.DateTimeField(auto_now_add=True)
+	STATUS_TYPES = (
+		("OP", "Open"),
+		("CL", "Closed"),
+		("RD", "Ready to be served")
+	)
+	status = models.CharField(max_length=2, choices=STATUS_TYPES)
 
 	def __str__(self):
 		return "Order %i" % self.id
+
+
+# feedback
+class Feedback(models.Model):
+	order = models.ForeignKey(Order)
+	message = models.TextField()
+
+# invoice
+class Invoice(models.Model):
+	order = models.ForeignKey(Order)
+	subtotal = models.DecimalField(decimal_places=2, max_digits=10)
+	tax = models.DecimalField(decimal_places=2, max_digits=5)
+	tip = models.DecimalField(decimal_places=2, max_digits=5)
+	comped = models.BooleanField(default=False)
+	total = models.DecimalField(decimal_places=2, max_digits=10)
+	# I think we should only be concerned with the split in the interface
+	# but for the sake of completeness, I'll add this
+	split_ways = models.IntegerField()
+	# the split is recorded in the Split model
+
+class Split(models.Model):
+	invoice = models.ForeignKey(Invoice)
+	amount = models.DecimalField(decimal_places=2, max_digits=10)
+
+class Employee(models.Model):
+	name = models.CharField(max_length=512)
+	is_manager = models.BooleanField(default=False)
+	password = models.CharField(max_length=32)
+
+# these could be implemented as boolean fields in the Employee class, but
+# this way, if there are specific things we need to store for waiters or cooks
+# this makes it easier
+class Waiter(models.Model):
+	employee = models.ForeignKey(Employee)
+	tables = models.ManyToManyField(Table)
+
+class Cook(models.Model):
+	employee = models.ForeignKey(Employee)
