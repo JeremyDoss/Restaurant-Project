20b4140298b82bd128bf6cf8779a4269718e350e
pipmix <onloadx@yahoo.com>
2015-04-13 23:01:42 -0500

    update

--- restaurant/core/unit_tests.py
+++ restaurant/core/unit_tests.py
@@ -33,15 +33,6 @@ class MenuTests(TestCase):
 		ordr = Order.objects.get(pk=1)
 		self.assertEqual(ordr.id, 1)
 
-	def test_order_time(self):
-		order = Order.objects.get(pk=1)
-		self.assertEqual(order.time, "") #checks for empty string
-
-	def test_order_status_open(self):
-		order = Order.objects.get(pk=1)
-		order.status='OP'
-		self.assertEqual(order.status, "OP")
-
 	def test_order_status_occupied(self):
 		order = Order.objects.get(pk=1)
 		order.status='OC'
@@ -84,32 +75,94 @@ class MenuTests(TestCase):
 
 	def test_game_table_split_to_one(self):
 		order = Order.objects.get(pk=1)
-		self.assertEqual(table.check.1.hasItems == true)
+		self.assertEqual(table.check1.hasItems == true)
 
 	def test_game_table_split_to_two(self):
 		order = Order.objects.get(pk=2)
-		self.assertEqual(table.check.2.hasItems == true)
+		self.assertEqual(table.check2.hasItems == true)
 
 	def test_game_table_split_to_three(self):
 		order = Order.objects.get(pk=3)
-		self.assertEqual(table.check.3 == true)
+		self.assertEqual(table.check3 == true)
 
 	def test_game_table_split_to_four(self):
 		order = Order.objects.get(pk=4)
-		self.assertEqual(table.check.4.hasItems == true)
+		self.assertEqual(table.check4.hasItems == true)
 
 	def test_game_table_split_one_hasPaid(self):
 		order = Order.objects.get(pk=1)
-		self.assertEqual(table.check.1.hasPaid == true)
+		self.assertEqual(table.check1.hasPaid == true)
 
 	def test_game_table_split_two_hasPaid(self):
 		order = Order.objects.get(pk=1)
-		self.assertEqual(table.check.2.hasPaid == true)
+		self.assertEqual(table.check2.hasPaid == true)
 
 	def test_game_table_split_three_hasPaid(self):
 		order = Order.objects.get(pk=1)
-		self.assertEqual(table.check.3.hasPaid == true)
+		self.assertEqual(table.check3.hasPaid == true)
 
 	def test_game_table_split_four_hasPaid(self):
 		order = Order.objects.get(pk=1)
-		self.assertEqual(table.check.4.hasPaid == true)
+		self.assertEqual(table.check4.hasPaid == true)
+
+
+class TableTests(TestCase):
+	def setUp(self):
+        	menu_item=MenuItem(
+            	name = "Water",
+            	category = Category.objects.create(parent=Category.objects.create(parent=None, name="Drinks"), name="Water"),
+            	times_ordered = 1,
+            	price = 0.00,
+            	description = "Water",
+            	calories = 0,
+            	sodium_mg = 0,
+            	fat_grams= 0,
+            	is_vegetarian = True,
+            	allergens="NONE"
+        	)
+        	ingredient = []
+        	ingredient.append(Ingredient.objects.create(name="water"))
+        	menu_item.save()
+        	menu_item.ingredients.add(ingredient[0])
+        	menu_item.save()
+
+	def test_order_tab;e(self):
+		Order.table = "1"
+		self.assertEqual (Order.table, "1")
+
+	def test_order_menu_items(self):
+		Order.menu_items = "Water"
+		self.assertEqual (Order.menu_items, "Water")
+
+	def test_order_status_open(self):
+		Order.status = "OP"
+		self.assertEqual (Order.status, "OP")
+
+	def test_order_status_closed(self):
+		Order.status = "CL"
+		self.assertEqual (Order.status, "CL")
+
+	def test_order_status_ready(self):
+		Order.status = "RD"
+		self.assertEqual (Order.status, "RD")
+
+	def test_waiter_employee(self):
+		Waiter.employee = "Jill"
+		self.assertEqual (Waiter.employee, "Jill")
+
+	def test_waiter_tables(self):
+		Waiter.table = "4"
+		self.assertEqual (Waiter.table, "4")
+
+	def test_cook_employee(self):
+		Cook.employee = "Jan"
+		self.assertEqual (Cook.employee, "Jan")
+
+	def test_order_time(self):
+		order = Order.objects.get(pk=1)
+		self.assertEqual(order.time, "") #checks for empty string
+
+	def test_order_status_open(self):
+		order = Order.objects.get(pk=1)
+		order.status='OP'
+		self.assertEqual(order.status, "OP")
